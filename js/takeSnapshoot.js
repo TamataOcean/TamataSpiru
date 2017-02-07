142e711473640bb32f668a18b6757a84a4b1523c
romaintourte <contact@tamataocean.com>
2017-02-07 21:34:08 +0100

    takeSnapshoot.js dev

--- js/takeSnapshoot.js
+++ js/takeSnapshoot.js
@@ -1,37 +1,54 @@
 var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
+var jsonfile = require('jsonfile');
 
 /* CONFIG for res.render to ejs Front End Files */
 /* -------------------------------------------- */
 var configFile = './config/config.json';
+var cameraOptions = {};
 
+jsonfile.readFile(configFile, function(err, obj) {
+	if (err) console.err(err);
+	cameraOptions = {
+		width       : obj.camera.width,
+		height      : obj.camera.height,
+		mode        : obj.camera.mode,
+		awb         : obj.camera.awb,
+		output      : obj.camera.output,
+		q           : obj.camera.q,
+		rot         : obj.camera.rot,
+		nopreview   : obj.camera.nopreview,
+		timeout     : obj.camera.timeout,
+		timelapse   : obj.camera.timelapse,
+		th          : obj.camera.th
+	};
+	
+	var camera = new require("raspicam")(cameraOptions);
+	camera.on("stop", function(){
+		console.log("Stop Camera");
+	})
+	
+	.on("start", function (){
+		console.log("Start Camera");
+	})
+	
+	.on("exit", function() {
+		camera.stop();
+		console.log('Restarting camera...')
+		camera.start()
+	});
+	camera.start();
+});
+	
 app.use(express.static(__dirname + '/images'));
 http.listen(8888, function(){
     console.log('Running...');
 });
  
-var cameraOptions = {
-    width       : 1024,
-    height      : 920,
-    mode        : "timelapse",
-    awb         : 'cloud',
-    output      : 'images/camera.jpg',
-    q           : 50,
-    rot         : 180,
-    nopreview   : true,
-    timeout     : 1000,
-    timelapse   : 999,
-    th          : "0:0:0"
-};
- 
-
- 
-var camera = new require("raspicam")(cameraOptions);
-camera.start();
- 
 app.get('/', function(req, res)
 {
+	console.log('get / ');
     res.sendFile(__dirname + '/images/camera.jpg');
 });
 
