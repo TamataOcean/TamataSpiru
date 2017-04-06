646db44599c97080e1cf119856f24ac7cc596637
Tamata Ocean <contact@tamataocean.com>
2017-04-06 15:35:35 +0200

    ADD - Snapshoot file

--- systools/tamataSpiruCronTab.sh
+++ systools/tamataSpiruCronTab.sh
@@ -1,2 +1,2 @@
-node /home/pi/node/tamataspiru/tamataCoolSpiru.js 
-node /home/pi/node/tamataspiru/tamataFarm.js 
+#node /home/pi/node/tamataspiru/tamataCoolSpiru.js 
+#node /home/pi/node/tamataspiru/tamataFarm.js 
